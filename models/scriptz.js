15b71565c101125658cf36ded665f67901794be1
chris <chrisrogers1017@gmail.com>
2018-03-20 20:58:57 -0600

    fixed an error

--- models/scriptz.js
+++ models/scriptz.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
         allowNull: false
       },
       when: {
-          type: DataTypes.String,
+          type: DataTypes.STRING,
           allowNull: false
       }
     });
